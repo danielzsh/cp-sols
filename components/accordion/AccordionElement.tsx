3115e321a0caeed40b590ce3b63cb8ca8c439827
devo1ution <danielfangzhu@gmail.com>
2023-05-13 09:05:04 -0700

    accordion is smoothhhh baby

--- components/accordion/AccordionElement.tsx
+++ components/accordion/AccordionElement.tsx
@@ -3,74 +3,62 @@ import styles from './accordion.module.css';
 import { motion } from 'framer-motion';
 
 export const AccordionElement = ({ children, label } : {children: ReactNode, label: string}) => {
-    const [open, setOpen] = useState(true);
-    const [height, setHeight] = useState('0px');
+    const [open, setOpen] = useState(false);
     const content = useRef(null);
-    useEffect(() => {
-        setHeight(content.current.scrollHeight + 'px');
-        setOpen(false);
-    }, []);
-    useEffect(() => {
-        if (open) content.current.style.maxHeight = height;
-        else content.current.style.maxHeight = 0;
-    }, [open]);
-    return <div className={styles.accordion}>
-        <button 
-            className={styles['accordion-button']} 
-            style={{display: 'flex', alignItems: 'center'}} 
-            onClick={() => {
-                setOpen(!open);
-            }}
+    return (
+        <motion.div 
+            className={styles.accordion}
+            animate={open ? "open" : "closed"}
         >
-            <motion.svg 
-                height="32" 
-                width="32" 
-                viewBox="0 0 20 20"
-                animate={open ? "open" : "closed"}
-                variants={{
-                    open: {rotate: 90},
-                    closed: {rotate: 0}
+            <button 
+                className={styles['accordion-button']} 
+                style={{display: 'flex', alignItems: 'center'}} 
+                onClick={() => {
+                    setOpen(!open);
                 }}
             >
-                <motion.path 
-                    d="M5 17 L5 3 L15 10 L5 17" 
-                    transition={{duration: 0.5}}
+                <motion.svg 
+                    height="32" 
+                    width="32" 
+                    viewBox="0 0 20 20"
                     variants={{
-                        open: {
-                            stroke: "black",
-                            fill: "rgba(0, 0, 0, 1)"
-                        },
-                        closed: {
-                            stroke: "grey",
-                            fill: "rgba(0, 0, 0, 0)"
-                        }
+                        open: {rotate: 90},
+                        closed: {rotate: 0}
                     }}
-                />
-                {/* <motion.path 
-                    d="M5 17 L5 3 L15 10 L5 17"
-                    style={{stroke: 'red'}} 
-                    variants={{
-                        open: {
-                            pathLength: 1,
-                            fill: "rgba(0, 0, 0, 1)"
-                        },
-                        closed: {
-                            pathLength: 0,
-                            fill: "rgba(0, 0, 0, 0)"
-                        }
-                    }}
-                    transition={{duration: 1, pathLength: {duration: 0.5}}}
-                /> */}
-            </motion.svg>
-            {label}
-        </button>
-        <div 
-            className={`${styles['accordion-content']} ${open ? styles.visible : styles.hidden}`} 
-            ref={content}
-        >
-            {children}
-        </div>
-    </div>
+                >
+                    <motion.path 
+                        d="M5 17 L5 3 L15 10 L5 17" 
+                        transition={{duration: 0.5}}
+                        variants={{
+                            open: {
+                                stroke: "black",
+                                fill: "rgba(0, 0, 0, 1)"
+                            },
+                            closed: {
+                                stroke: "grey",
+                                fill: "rgba(0, 0, 0, 0)"
+                            }
+                        }}
+                    />
+                </motion.svg>
+                {label}
+            </button>
+            <motion.div 
+                className={`${styles['accordion-content']}`} 
+                ref={content}
+                variants={{
+                    open: {
+                        height: "auto"
+                    },
+                    closed: {
+                        height: 0
+                    }
+                }}
+            >
+                {children}
+            </motion.div>
+        </motion.div>
+    )
 }
 
 AccordionElement.defaultProps = {
