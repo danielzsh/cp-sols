f11210212686ebf42b4740ef29178825b4c783cb
devo1ution <danielfangzhu@gmail.com>
2023-05-18 11:53:11 -0700

    byepress

--- components/accordion/AccordionElement.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { AccordionElement } from './AccordionElement'
-
-describe('<AccordionElement />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<AccordionElement label="button">content</AccordionElement>)
-  })
-})
