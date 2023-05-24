8ff518621c5f11317d3c2d49d32e07ac64770672
devo1ution <danielfangzhu@gmail.com>
2023-05-23 17:58:43 -0700

    improve page

--- app/content/[...slug]/page.tsx
+++ app/content/[...slug]/page.tsx
@@ -1,10 +1,7 @@
-import dynamic from "next/dynamic";
-const getDynamicComponent = (c) => dynamic(() => import(`../${c}/page.mdx`), {
-    ssr: false,
-    loading: () => <p>Loading...</p>,
-});
-export default function Page({ params }: { params: { slug: string[] } }) {
+export default async function Page({ params }: { params: { slug: string[] } }) {
     const path = params.slug.join("/");
-    const Component = getDynamicComponent(path);
+    const {
+        default: Component
+    } = await import(`../${path}/page.mdx`);
     return <Component />
 }
